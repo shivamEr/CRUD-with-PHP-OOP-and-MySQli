0db5bae491adefa839285b69cd2ac06f5ec247d4
shivam kumar <shivamksbabu504@gmail.com>
2024-04-12 17:34:34 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD-with- PHP-OOP and MySQLi</title>
+</head>
+<body>
+    
+</body>
+</html>
